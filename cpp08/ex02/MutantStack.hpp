8ad82ed6764968d43a64510c2afb66653bc11ba8
ADEK iMac Pro 1 <adek@ADEKs-iMac-Pro.local>
2024-01-23 08:31:02 +0400

    added reverse iterators to cpp08

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -22,10 +22,18 @@ public:
 
     typedef typename Container::iterator    iterator;
     typedef typename Container::const_iterator    const_iterator;
+	typedef typename Container::reverse_iterator reverse_iterator;
+	typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
     iterator    begin() { return this->c.begin(); }
     iterator    end() { return this->c.end(); }
 
-    const_iterator    cbegin() const { return this->c.begin(); }
-    const_iterator    cend() const { return this->c.end(); }
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+
+    const_iterator    begin() const { return this->c.begin(); }
+    const_iterator    end() const { return this->c.end(); }
+
+	const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+	const_reverse_iterator rend() const { return this->c.rend(); }
 };
